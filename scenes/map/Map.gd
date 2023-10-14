30d4bbbabbfe331b11a9fc8613af71ef3e42bdb1
YaYana-K <yanulja17@gmail.com>
2023-10-15 00:21:49 +0300

    Кнопка паузи, сірий екран для паузи

--- scenes/map/Map.gd
+++ scenes/map/Map.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @onready var map = $TileMap
 @onready var ship = $TileMap/Ship
+@onready var pause_menu = %PauseMenu as PauseMenu
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,6 +12,7 @@ func _ready():
 	var user_position = SceneSwitcher.get_param('respawn_position')
 	if user_position != null:
 		ship.position = user_position
+	pause_menu.connect("pressed_exit_button", exit_from_pause)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -21,3 +23,5 @@ func _on_click(_position: Vector2):
 	pass
 	#ship.navigate(_position)
 
+func exit_from_pause():
+	get_tree().call_deferred("quit")
--- scenes/pause_menu/PauseMenu.gd
+++ scenes/pause_menu/PauseMenu.gd
@@ -1,51 +1,55 @@
 extends Control
 
+class_name PauseMenu
+
 signal pressed_exit_button
 
 @onready var bus_index = AudioServer.get_bus_index("Music")
 @onready var sound_slider = $Panel/VBoxContainer/SliderContainer/VBoxContainer2/SoundSlider
-@onready var audio_player: AudioStreamPlayer =  $"../../AudioStreamPlayer"
-@onready var test_sound = %TestSoundPlayer
-
-@onready var pause_button = %PauseButton
+@onready var audio_player: AudioStreamPlayer = %TestSoundPlayer
+@onready var test_sound = %TestSoundPlayer as AudioStreamPlayer
+@onready var pause_button = %PauseButton as Control
+@onready var sound_timer = $Timer as Timer
 
 func  _ready():
 	hide()
-	PauseManager.connect("toggle_paused", _on_pause_manager_togle_paused)
-	PauseManager.connect("toggle_esc", _on_pause_manager_togle_esc)
-	pause_button.connect("pause_button_pressed", _on_pause_manager_togle_esc)
+	PauseManager.connect("toggle_paused", _on_pause_manager_toggle_paused)
+	PauseManager.connect("toggle_esc", _on_pause_manager_toggle_paused)
+	pause_button.connect("pause_button_pressed", _on_pause_manager_toggle_paused)
 	
 	var saved_volume = load_volume()
 	sound_slider.value = saved_volume
 	AudioServer.set_bus_volume_db(bus_index, linear_to_db(saved_volume))
 	test_sound.bus = "Music"
 
-func _on_pause_manager_togle_paused(is_paused: bool):
+func _on_pause_manager_toggle_paused(is_paused: bool):
 	if is_paused:
 		show()
+		audio_player.stream_paused = true
 	else :
 		hide()
-
-func _on_pause_manager_togle_esc(is_paused: bool):
-	if is_paused:
-		audio_player.stop()
-	else :
-		audio_player.play()
+		audio_player.stream_paused = false
 
 func _on_play_pressed():
 	PauseManager.game_paused = false
-	audio_player.play()
+	audio_player.stream_paused = false
 
 func _on_exit_pressed():
 	emit_signal("pressed_exit_button")
 	PauseManager.game_paused = false
-	await get_tree().create_timer(0.5).timeout
-	audio_player.play()
+	await get_tree().create_timer(3.0).timeout
+	audio_player.stream_paused = false
 
 func _on_sound_slider_value_changed(value):
-	test_sound.play()
 	AudioServer.set_bus_volume_db(bus_index,linear_to_db(value))
 	save_volume(value)
+	play_sound()
+
+func play_sound():
+	sound_timer.start()
+	await sound_timer.timeout
+	test_sound.play()
+	sound_timer.stop()
 
 func save_volume(volume: float):
 	var config = ConfigFile.new()
@@ -58,3 +62,8 @@ func load_volume() -> float:
 	if config.load("user://config.cfg") == OK:
 		return config.get_value("audio", "volume", 0.5)
 	return 0.5
+
+func _notification(what):
+	match what:
+		NOTIFICATION_WM_WINDOW_FOCUS_OUT:
+			PauseManager.game_paused = true
