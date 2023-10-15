af1ef0640ea3fce54d468d6d6fe7ca2db87da918
YaYana-K <yanulja17@gmail.com>
2023-10-15 14:01:50 +0300

    Виправила лишній звук при переході між сценами
    Перенесла збереження гучності в глобальний скрипт

--- scenes/map/Map.gd
+++ scenes/map/Map.gd
@@ -12,8 +12,9 @@ func _ready():
 	var user_position = SceneSwitcher.get_param('respawn_position')
 	if user_position != null:
 		ship.position = user_position
+		
 	pause_menu.connect("pressed_exit_button", exit_from_pause)
-
+	var saved_volume = PauseManager.load_volume()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -24,4 +25,4 @@ func _on_click(_position: Vector2):
 	#ship.navigate(_position)
 
 func exit_from_pause():
-	get_tree().call_deferred("quit")
+	get_tree().quit()
--- scenes/pause_menu/PauseMenu.gd
+++ scenes/pause_menu/PauseMenu.gd
@@ -6,7 +6,7 @@ signal pressed_exit_button
 
 @onready var bus_index = AudioServer.get_bus_index("Music")
 @onready var sound_slider = $Panel/VBoxContainer/SliderContainer/VBoxContainer2/SoundSlider
-@onready var audio_player: AudioStreamPlayer = %TestSoundPlayer
+@onready var audio_player: AudioStreamPlayer = %AudioStreamPlayer
 @onready var test_sound = %TestSoundPlayer as AudioStreamPlayer
 @onready var pause_button = %PauseButton as Control
 @onready var sound_timer = $Timer as Timer
@@ -17,7 +17,7 @@ func  _ready():
 	PauseManager.connect("toggle_esc", _on_pause_manager_toggle_paused)
 	pause_button.connect("pause_button_pressed", _on_pause_manager_toggle_paused)
 	
-	var saved_volume = load_volume()
+	var saved_volume = PauseManager.load_volume()
 	sound_slider.value = saved_volume
 	AudioServer.set_bus_volume_db(bus_index, linear_to_db(saved_volume))
 	test_sound.bus = "Music"
@@ -28,40 +28,26 @@ func _on_pause_manager_toggle_paused(is_paused: bool):
 		audio_player.stream_paused = true
 	else :
 		hide()
-		audio_player.stream_paused = false
 
 func _on_play_pressed():
 	PauseManager.game_paused = false
 	audio_player.stream_paused = false
 
 func _on_exit_pressed():
-	emit_signal("pressed_exit_button")
+	audio_player.stop()
 	PauseManager.game_paused = false
-	await get_tree().create_timer(3.0).timeout
-	audio_player.stream_paused = false
+	emit_signal("pressed_exit_button")
 
 func _on_sound_slider_value_changed(value):
-	AudioServer.set_bus_volume_db(bus_index,linear_to_db(value))
-	save_volume(value)
-	play_sound()
-
-func play_sound():
 	sound_timer.start()
+	play_test_sound()
+	AudioServer.set_bus_volume_db(bus_index,linear_to_db(value))
+	PauseManager.save_volume(value)
+
+func play_test_sound():
 	await sound_timer.timeout
 	test_sound.play()
 	sound_timer.stop()
-
-func save_volume(volume: float):
-	var config = ConfigFile.new()
-	config.load("user://config.cfg")
-	config.set_value("audio", "volume", volume)
-	config.save("user://config.cfg")
-
-func load_volume() -> float:
-	var config = ConfigFile.new()
-	if config.load("user://config.cfg") == OK:
-		return config.get_value("audio", "volume", 0.5)
-	return 0.5
 
 func _notification(what):
 	match what:
