406da527bf32968f122d1ca18c41eef146651ab8
YaYana-K <yanulja17@gmail.com>
2023-11-15 20:05:27 +0200

    Додала ціну до скінів і відображення її на кнопках

--- scenes/shop/slot.gd
+++ scenes/shop/slot.gd
@@ -5,13 +5,16 @@ signal slot_pressed(type_body, type_skin)
 @onready var price_btn: Button = $PriceBtn
 @onready var icon: Sprite2D = $Panel/Icon
 var region
+var type_body
+var type_skin
 
 func _ready() -> void:
 	region = icon.region_rect
+	find_skin_keys(region)
+	price_btn.text = str(Skins.get_skin_price(type_body, type_skin))
 
-func find_skin_keys(region_rect):
-	var type_body
-	var type_skin
+func find_skin_keys(region_rect) -> Array:
+	
 	for type_key in Skins.skins.keys():
 		var type_skins = Skins.skins[type_key]
 		
@@ -21,11 +24,11 @@ func find_skin_keys(region_rect):
 				type_body = type_key
 				type_skin = skin_type_key
 				break
-	emit_signal("slot_pressed", type_body, type_skin)
+	return [type_body, type_skin]
+	
 
 func _on_button_pressed() -> void:
-	find_skin_keys(region)
-	#print(new_skin)
+	emit_signal("slot_pressed", type_body, type_skin)
 	pass
 
 
